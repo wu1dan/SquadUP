27452c5a15b240a4c199ceef570f9f806972d314
wu1dan <wu1danny@gmail.com>
2019-11-05 14:27:39 -0800

    Codacy updates are done.

--- app/src/main/java/com/example/squadup/Profile.java
+++ app/src/main/java/com/example/squadup/Profile.java
@@ -46,7 +46,7 @@ public class Profile extends AppCompatActivity {
         btnEditProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Profile.this, createprofile.class);
+                Intent intent = new Intent(Profile.this, CreateProfile.class);
                 startActivity(intent);
             }
         });
